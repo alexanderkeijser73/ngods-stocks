c94069667522f6312130c455c3a0e945d394cd8f
alexander.keijser <alexanderkeijser73@gmail.com>
2024-07-14 20:32:20 +0900

    Fix python dependencies and outdated library imports

--- projects/dagster/workspace.py
+++ projects/dagster/workspace.py
@@ -1,14 +1,24 @@
 from dagster import job, repository
 from dagster_dbt import dbt_cli_resource
 from dagster_pyspark import pyspark_resource
-
-from db import initialize_db, drop_tables_op, create_schemas_op, trino_resource
-from download import download_yahoo_finance_files, download_yahoo_finance_files_op
-from dbt import dbt_bronze, dbt_silver, dbt_gold, dbt_all, dbt_bronze_run_op, dbt_silver_run_op, dbt_gold_run_op, dbt_bronze_test_doc_sources_op, dbt_silver_test_doc_sources_op, dbt_gold_test_doc_sources_op
+from db import create_schemas_op, drop_tables_op, initialize_db, trino_resource
+from dbt_ import (dbt_all, dbt_bronze, dbt_bronze_run_op,
+                  dbt_bronze_test_doc_sources_op, dbt_gold, dbt_gold_run_op,
+                  dbt_gold_test_doc_sources_op, dbt_silver, dbt_silver_run_op,
+                  dbt_silver_test_doc_sources_op)
+from download import (download_yahoo_finance_files,
+                      download_yahoo_finance_files_op)
 from predict import predict_op
 
-@job(resource_defs={'dbt': dbt_cli_resource, 'trino': trino_resource, 'pyspark': pyspark_resource})
-def e2e():    
+
+@job(
+    resource_defs={
+        "dbt": dbt_cli_resource,
+        "trino": trino_resource,
+        "pyspark": pyspark_resource,
+    }
+)
+def e2e():
     dbt_gold_test_doc_sources_op(
         dbt_gold_run_op(
             predict_op(
@@ -17,9 +27,7 @@ def e2e():
                         dbt_bronze_test_doc_sources_op(
                             dbt_bronze_run_op(
                                 download_yahoo_finance_files_op(
-                                    drop_tables_op(
-                                        create_schemas_op()
-                                    )
+                                    drop_tables_op(create_schemas_op())
                                 )
                             )
                         )
@@ -30,7 +38,7 @@ def e2e():
     )
 
 
-@job(resource_defs={'pyspark': pyspark_resource})
+@job(resource_defs={"pyspark": pyspark_resource})
 def predict():
     predict_op()
 
@@ -38,12 +46,12 @@ def predict():
 @repository
 def workspace():
     return [
-        initialize_db, 
+        initialize_db,
         download_yahoo_finance_files,
         dbt_bronze,
         dbt_silver,
         dbt_gold,
         dbt_all,
         e2e,
-        predict
+        predict,
     ]
